6453a20c8adc3506d29c3565d6b96be126f5890d
Piotr <pm@gmail.com>
2017-10-23 11:52:51 +0700

    Change PropertiesParser into more genetic way.

--- src/test/java/org/prosky/ghost/selenium/baseFramework/PropertiesParser.java
+++ src/test/java/org/prosky/ghost/selenium/baseFramework/PropertiesParser.java
@@ -1,102 +1,88 @@
 package org.prosky.ghost.selenium.baseFramework;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 import org.openqa.selenium.By;
-import org.testng.log4testng.Logger;
 
-public class UserInterfaceMapParser {
-	private static final Logger log = Logger.getLogger(UserInterfaceMapParser.class);
-	private FileInputStream input;
-	private Properties propertyFile = new Properties();
+public class PropertiesParser {
+    private static final Logger LOG = Logger.getLogger(PropertiesParser.class.getName());
+    private InputStream input;
+    private Properties propertyFile = new Properties();
 
-	public UserInterfaceMapParser(String fileName) throws IOException {
-		try {
-			Path path = Paths.get(fileName);
-			log.info("Reading config from file: " + path.toAbsolutePath().toString());
-			input = new FileInputStream(path.toAbsolutePath().toString());
-			propertyFile.load(input);
-		} catch (IOException ex) {
-			ex.printStackTrace();
-			throw ex;
-		} finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-					throw e;
-				}
-			}
-		}
+    public PropertiesParser(String fileName) throws IOException {
+	try {
+	    LOG.info(getClass().getClassLoader().getResource(fileName).toString());
+	    input = getClass().getClassLoader().getResourceAsStream(fileName);
+	    propertyFile.load(input);
+	} finally {
+	    if (input != null) {
+		input.close();
+	    }
 	}
 
-	public UserInterfaceMapParser() throws IOException {
-		try {
-			Path path = Paths.get("user-interface-map.properties");
-			log.info("Reading config from file: " + path.toAbsolutePath().toString());
-			input = new FileInputStream(path.toAbsolutePath().toString());
-			propertyFile.load(input);
-		} catch (IOException ex) {
-			ex.printStackTrace();
-			throw ex;
-		} finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-					throw e;
-				}
-			}
-		}
+    }
+
+    public By getLocator(String locatorName) throws ArrayIndexOutOfBoundsException {
+	String locatorProperty;
+	String locatorType;
+	String locatorValue;
+	try {
+	    locatorProperty = propertyFile.getProperty(locatorName);
+	    locatorType = locatorProperty.split(":")[0];
+	    locatorValue = locatorProperty.split(":")[1];
+	} catch (ArrayIndexOutOfBoundsException e) {
+	    e.printStackTrace();
+	    throw e;
 	}
 
-	public By getLocator(String locatorName) throws ArrayIndexOutOfBoundsException {
-		String locatorProperty;
-		String locatorType;
-		String locatorValue;
-		try {
-			locatorProperty = propertyFile.getProperty(locatorName);
-			// log.info("Locator " + locatorName + " = " + locatorProperty.toString());
-			locatorType = locatorProperty.split(":")[0];
-			locatorValue = locatorProperty.split(":")[1];
-		} catch (ArrayIndexOutOfBoundsException e) {
-			e.printStackTrace();
-			throw e;
-		}
-
-		By locator = null;
-		switch (locatorType) {
-		case "ById":
-			locator = By.id(locatorValue);
-			break;
-		case "ByName":
-			locator = By.name(locatorValue);
-			break;
-		case "ByClassName":
-			locator = By.className(locatorValue);
-			break;
-		case "ByCssSelector":
-			locator = By.cssSelector(locatorValue);
-			break;
-		case "ByLinkText":
-			locator = By.linkText(locatorValue);
-			break;
-		case "ByPartialLinkText":
-			locator = By.partialLinkText(locatorValue);
-			break;
-		case "ByTagName":
-			locator = By.tagName(locatorValue);
-			break;
-		case "ByXPath":
-			locator = By.xpath(locatorValue);
-			break;
-		}
-		return locator;
+	By locator = null;
+	switch (locatorType) {
+	case "ById":
+	    locator = By.id(locatorValue);
+	    break;
+	case "ByName":
+	    locator = By.name(locatorValue);
+	    break;
+	case "ByClassName":
+	    locator = By.className(locatorValue);
+	    break;
+	case "ByCssSelector":
+	    locator = By.cssSelector(locatorValue);
+	    break;
+	case "ByLinkText":
+	    locator = By.linkText(locatorValue);
+	    break;
+	case "ByPartialLinkText":
+	    locator = By.partialLinkText(locatorValue);
+	    break;
+	case "ByTagName":
+	    locator = By.tagName(locatorValue);
+	    break;
+	case "ByXPath":
+	    locator = By.xpath(locatorValue);
+	    break;
 	}
+	return locator;
+    }
+
+    public Object[][] getTestData(String type1, String type2, int minExpectefNrOfTests) {
+	int nrOfTests = propertyFile.size() / 2;
+
+	Enumeration<?> e = propertyFile.propertyNames();
+	while (e.hasMoreElements()) {
+	    String key = (String) e.nextElement();
+	    String value = propertyFile.getProperty(key);
+	    System.out.println("Key : " + key + ", Value : " + value);
+	}
+
+	Object[][] testData = new Object[][] {
+		{ "admin@test.org", "admin123" },
+		{ "admin@prosky.org", "admin123" } };
+	System.out.println(testData[0][0]);
+	return testData;
+    }
 }
--- src/test/java/org/prosky/ghost/selenium/pageObjects/Page.java
+++ src/test/java/org/prosky/ghost/selenium/pageObjects/Page.java
@@ -7,43 +7,45 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.prosky.ghost.selenium.baseFramework.UserInterfaceMapParser;
+import org.prosky.ghost.selenium.baseFramework.PropertiesParser;
 
-public class Page extends UserInterfaceMapParser {
-	private final String defaultSiteUrl = "http://localhost:2368/";
-	protected final String baseUrl = System.getProperty("site.url", defaultSiteUrl);
-	protected WebDriver driver;
-	protected WebDriverWait wait;
+public class Page {
+    private final String defaultSiteUrl = "http://localhost:2368/";
+    protected final String baseUrl = System.getProperty("site.url", defaultSiteUrl);
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    private PropertiesParser uiMap;
 
-	public Page(WebDriver driver) throws IOException {
-		this.driver = driver;
-		wait = new WebDriverWait(driver, 10);
+    public Page(WebDriver driver) throws IOException {
+	this.driver = driver;
+	wait = new WebDriverWait(driver, 10);
+	uiMap = new PropertiesParser("user-interface-map.properties");
+    }
+
+    public void waitUntilFindElement(By by) {
+	wait.until(ExpectedConditions.numberOfElementsToBe(by, 1));
+    }
+
+    public void waitUntilFindElement(By by, int timeOut) {
+	WebDriverWait newWait = new WebDriverWait(driver, timeOut);
+	newWait.until(ExpectedConditions.numberOfElementsToBe(by, 1));
+    }
+
+    public void waitUntilFindElement(String locatorName) {
+	try {
+	    waitUntilFindElement(uiMap.getLocator(locatorName));
+
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    throw e;
 	}
+    }
 
-	public void waitUntilFindElement(By by) {
-		wait.until(ExpectedConditions.numberOfElementsToBe(by, 1));
-	}
+    public void waitUntilFindElement(String locatorName, int timeOut) {
+	waitUntilFindElement(uiMap.getLocator(locatorName), timeOut);
+    }
 
-	public void waitUntilFindElement(By by, int timeOut) {
-		WebDriverWait newWait = new WebDriverWait(driver, timeOut);
-		newWait.until(ExpectedConditions.numberOfElementsToBe(by, 1));
-	}
-
-	public void waitUntilFindElement(String locatorName) {
-		try {
-			waitUntilFindElement(getLocator(locatorName));
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
-
-	public void waitUntilFindElement(String locatorName, int timeOut) {
-		waitUntilFindElement(getLocator(locatorName), timeOut);
-	}
-
-	public WebElement findElement(String locatorName) {
-		return driver.findElement(getLocator(locatorName));
-	}
+    public WebElement findElement(String locatorName) {
+	return driver.findElement(uiMap.getLocator(locatorName));
+    }
 }
